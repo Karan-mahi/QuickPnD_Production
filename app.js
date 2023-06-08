bd8911bb5d2330854f11bc26ce3e3319e8f16ef2
Karan <karan3681.be20@chitkara.edu.in>
2023-06-09 00:56:39 +0530

    build update

--- app.js
+++ app.js
@@ -4,24 +4,25 @@ const path = require("path");
 const app = express();
 const cors = require("cors");
 
-app.use(express.static(path.resolve(__dirname, './build')));
+app.use(express.static(path.resolve(__dirname, "./build")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(
-    cors({
-      origin: "https://quick-pn-d-production.vercel.app",
-      credentials: true,
-    })
-  );
+app.use(cors());
 
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://quick-pn-d-production.vercel.app");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://quick-pn-d-production.vercel.app"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 ///Connecting Database
-require("./model/conn")
+require("./model/conn");
 
 app.use("/auth", require("./routes/auth"));
 app.use("/api", require("./routes/api"));
